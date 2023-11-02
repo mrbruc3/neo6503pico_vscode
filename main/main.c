720c032fa26d275df16466b672f483d719e93d8c
Mark Westmijze <mark.westmijze@gmail.com>
2023-11-02 10:24:06 +0100

    optimalisaties en stdio via uart en usb

--- main/main.c
+++ main/main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
-#include "hardware/uart.h"
+#include "pico/stdio.h"
+//#include "hardware/uart.h"
+
+//#define SLOW
+//#define DEBUG
 
 
 #define UART_ID uart0
 #define BAUD_RATE 115200
-
+/*
 // We are using pins 0 and 1, but see the GPIO function select table in the
 // datasheet for information on which other pins can be used.
 #define UART_TX_PIN 28
 #define UART_RX_PIN 29
-
+*/
 
 #define DEF_LED_PIN 23
 
@@ -25,37 +30,26 @@
 #define RW_PIN 11
 
 
-//#define TIME_DELTA sleep_us(1);
-//#define TIME_DELTA asm volatile("nop \nnop \nnop \nnop \n");
-#define TIME_DELTA asm volatile("nop \nnop \nnop \nnop \nnop \nnop \nnop \nnop \n");
-//#define TIME_DELTA sleep_ms(125);
-//#define TIME_DELTA_SMALL sleep_us(1);
 
-// 3 nops is current minimum
-//#define TIME_DELTA_SMALL asm volatile("nop \nnop \nnop \n");
+#ifdef SLOW
+#define TIME_DELTA sleep_ms(250);
+#define TIME_DELTA_SMALL sleep_us(1);
+#else
+#define TIME_DELTA asm volatile("nop \nnop \nnop \nnop \nnop \nnop \nnop \nnop \n");
 #define TIME_DELTA_SMALL asm volatile("nop \nnop \nnop \nnop \nnop \nnop \n");
+#endif
 
 #define TIME_DELTA_LARGE sleep_ms(1);
 
 uint8_t ram[1024];
 
-void print_raw_values(uint32_t vals, char * pre, int bits, char * post)
-{
- /*   uart_puts(UART_ID, pre);
-    
-    for (int i=0; i<bits; i++){
-        if (i == 8 || i == 16 || i == 24) {
-            uart_putc_raw(UART_ID, ' ');
-        }
-        uart_putc_raw(UART_ID, (vals >> i) & 0x1 ? '1' : '0');
-    }
-    uart_puts(UART_ID, post);*/
-}
-
 #define GPIO_MASK 0xFF
 
+uint64_t clks;
+uint64_t start;
 
 uint8_t mem[] = {
+0xae, 0x00, 0x04,
 0x18 ,     
 0xd8      ,
 0xa2, 0x00   ,
@@ -118,8 +112,8 @@ uint8_t mem[] = {
 0x86 , 0x07   ,
 0x86 , 0x08   ,
 0xa0 , 0xaa   ,
-0x84, 0x16, 
- 0x4c, 0x73, 0x06 
+0xae, 0x01, 0x04,
+0x4c, 0x76, 0x06 
 };
 
 
@@ -182,6 +176,34 @@ uint8_t read_from_mem(uint32_t address){
     switch (address) {
         case 0xfffc:    return base & 0xff;
         case 0xfffd:    return (base >> 8) & 0xff;
+        case 0x0400:    printf("start\n");
+                        clks = 0;
+                        start = time_us_64();
+                        break;
+        case 0x0401:    {
+                            uint64_t delta = time_us_64() - start;
+                            float mhz = ((float)clks / (float)delta);
+                            uint32_t it = ram[0] + (ram[1] << 8);
+                            uint32_t ans = (ram[6] << 16) + (ram[7] << 8) + ram[8];
+
+                            printf("delta us:     %6lld\n", delta);
+                            printf("clock cycles: %6lld\n",clks);
+                            printf("mhz:          %3f\n",mhz);
+                            printf("iterations:   %3d\n", it);
+                            printf("answer:       %6d\n", ans);    
+                                                   
+                            if (ans != 233168) {
+                                printf("ERROR\n");
+                            }
+                            else {
+                                printf("SUCCESS\n");
+                                while(true) {
+                                    sleep_ms(1000);
+                                }
+
+                            }
+                        }
+                        break;
     }
 
     return 0xbb;
@@ -213,34 +235,31 @@ void simulate_mem_read(uint32_t data){
 }
 
 int main() {
-    stdio_usb_init();
+    //stdio_usb_init();
 
-    
+    //set_sys_clock_khz(125000, true);
+    set_sys_clock_khz(250000, true);
 
-
-    uint64_t start = time_us_64();
+    stdio_init_all();
 
     // Set up our UART with the required speed.
-    uart_init(UART_ID, BAUD_RATE);
-
+    //uart_init(UART_ID, BAUD_RATE);
+/*
     while (!stdio_usb_connected()){
     }
-
-    printf("Start over USB\n");
+*/
+    printf("\nStart over USB\n");
 
     for (int i=0; i<sizeof(ram); i++) {
         ram[i] = 0xaa;
     }
 
-    //set_sys_clock_khz(125000, true);
-    //set_sys_clock_khz(32000, true);
-    set_sys_clock_khz(250000, true);
-    //printf("clock: %d\n", get_clock_);
+
 
     // Set the TX and RX pins by using the function select on the GPIO
     // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    //gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    //gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
     gpio_init(DEF_LED_PIN);
     gpio_set_dir(DEF_LED_PIN, GPIO_OUT);
@@ -284,16 +303,12 @@ int main() {
         set_clock(1);
     }
 
-    TIME_DELTA_SMALL;
+    TIME_DELTA_SMALL
     gpio_put(RESET_PIN, 1);
     // time before clock fall
-    TIME_DELTA_SMALL;
+    TIME_DELTA_SMALL
 
-    uint32_t prev_ans = 0;
-    uint64_t clks = 0;
-
-
-    uart_puts(UART_ID, "\r\nSTART \r\n");
+    //uart_puts(UART_ID, "\r\nSTART \r\n");
     while (true) {
         
         // CLOCK FALL
@@ -322,27 +337,13 @@ int main() {
         else {
             mem_output = read_from_proc(sampled_address);
         }
+        //TIME_DELTA
 
 
-        uint32_t it = ram[0] + (ram[1] << 8);
-        uint32_t ans = (ram[6] << 16) + (ram[7] << 8) + ram[8];
-
-        if (prev_ans != ans) {
-            prev_ans = ans;
-        
-            if (ans == 0 || ans == 233168 || it > 1000) {
-
-            if (ans == 0) {
-                start =  time_us_64();
-                clks = 0;
-            }
+#ifdef DEBUG
+        printf("CLK: %6lld A: %04x RW:%c D: %04x\n", 
+                clks,  sampled_address, control == 1 ? 'r': 'w', mem_output);
             
-            uint64_t delta = time_us_64() - start;
-            float us = (float)delta / (float)clks;
-
-            printf("%6lld %6lld %3f %3d %6d) A: %04x RW:%c D: %04x\n", 
-                delta, clks,  us, it, ans, sampled_address, control == 1 ? 'r': 'w', mem_output);
-            }
-        }
+#endif
     }
 }
